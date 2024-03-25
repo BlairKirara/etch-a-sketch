9b1f34439201d9a831fa266dd1a35cde49972a02
BlairKirara <bluberry.muffin.15@gmail.com>
2024-03-25 15:06:18 +0100

    Put the logic for grid creation in a functions

--- js.js
+++ js.js
@@ -1,12 +1,34 @@
 const containter = document.getElementById('container');
 const gridSizeButton = document.getElementById('gridSizeButton');
-let gridSize = 256;
+const kwadraty = document.getElementsByClassName('kwadraty');
+let gridSize = 16;
+let containerWidth = 500;
 
-for(let i = 0; i<gridSize; i++){
-    const div = document.createElement('div');
-    div.classList.add('kwadraty');
-    containter.append(div);
+function createGrid(){
+          for(let i = 0; i<gridSize; i++){
+            for(let j=0; j<gridSize; j++){
+                const div = document.createElement('div');
+                div.classList.add('kwadraty');
+                containter.append(div);
+            }
+        }
+        for(let i = kwadraty.length - 1; i >= 0; --i)
+        {
+            kwadraty[i].style.width = (containerWidth/gridSize) + "px";
+            kwadraty[i].style.height = (containerWidth/gridSize) + "px";
+
+        }
 }
+
+function randomRGB(){
+  let num1 = Math.random() * (255 - 1) + 1;
+  let num2 = Math.random() * (255 - 1) + 1;
+  let num3 = Math.random() * (255 - 1) + 1;
+}
+
+randomRGB();
+
+createGrid();
 
 
     containter.addEventListener(
@@ -25,11 +47,11 @@ for(let i = 0; i<gridSize; i++){
               }
             
           gridSize = prompt("Enter a desired nxn grid size. It should be a number between 10 and 100.")
-          for(let i = 0; i<gridSize; i++){
-            const div = document.createElement('div');
-            div.classList.add('kwadraty');
-            containter.append(div);
-        }
+          while(gridSize>100 || gridSize<10){
+            gridSize = prompt("It should be a number between 10 and 100.")
+          }
+          
+          createGrid();
         }
       );
   
