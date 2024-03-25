9aa7c9dce1e2d7cbec350e975c90713c69a4747e
BlairKirara <bluberry.muffin.15@gmail.com>
2024-03-25 13:47:40 +0100

    Add an option that deletes and creates grid

--- js.js
+++ js.js
@@ -1,27 +1,35 @@
 const containter = document.getElementById('container');
-// const div1 = document.createElement('div');
-// const div2 = document.createElement('div');
-// const div3 = document.createElement('div');
-// const div4 = document.createElement('div');
-// const div5 = document.createElement('div');
-// const div6 = document.createElement('div');
+const gridSizeButton = document.getElementById('gridSizeButton');
+let gridSize = 256;
 
-// containter.append(div1);
-// containter.append(div2);
-// containter.append(div3);
-// containter.append(div4);
-// containter.append(div5);
-// containter.append(div6);
-
-// div1.classList.add('kwadraty');
-// div2.classList.add('kwadraty');
-// div3.classList.add('kwadraty');
-// div4.classList.add('kwadraty');
-// div5.classList.add('kwadraty');
-// div6.classList.add('kwadraty');
-
-for(let i = 0; i<256; i++){
+for(let i = 0; i<gridSize; i++){
     const div = document.createElement('div');
     div.classList.add('kwadraty');
     containter.append(div);
 }
+
+
+    containter.addEventListener(
+        "mouseover",
+        (event) => {
+          // highlight the mouseover target
+          event.target.style.backgroundColor = "orange";
+        }
+      );
+
+      gridSizeButton.addEventListener(
+        "click",
+        () => {
+            while (containter.firstChild) {
+                containter.removeChild(containter.firstChild);
+              }
+            
+          gridSize = prompt("Enter a desired nxn grid size. It should be a number between 10 and 100.")
+          for(let i = 0; i<gridSize; i++){
+            const div = document.createElement('div');
+            div.classList.add('kwadraty');
+            containter.append(div);
+        }
+        }
+      );
+  
